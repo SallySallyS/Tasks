287e7a82cde88156256aa3232c23023ffb119c99
Елизавета <sova.lizochka05@gmail.com>
2024-11-20 21:38:01 +0300

    Add files via upload

---
+++ Task1_19.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <iostream>
+/*Указать значение величины x после выполнения следующих операторов при -
+сваивания:
+а)
+    x:= 10
+    x:= -10
+б)
+    x:= 17.5
+    x:= -2 * x
+в)
+    x:= 60
+    x:= x-1
+    x:= 0
+г)
+    x:= -50
+    x:= -25
+    x:= x+k 
+*/
+//using namespace std;
+int main()
+{
+    {
+        int x = 10;
+        x = -10;
+        std::cout << x << std::endl;
+    }
+    {
+        double x = 17.5;
+        x = -2 * x;
+        std::cout << x << std::endl;
+    }
+    {
+        int x = 60;
+        x = x - 1;
+        x = 0;
+        std::cout << x << std::endl;
+    }
+    {
+        int k = 9;
+        int x = -50;
+        x = -25;
+        x = x + k;
+        std::cout << x << std::endl;
+    }
+    return 0;
+
+}
+
