c2638609b839acc6b1fe83b98cb2f5dadc86f2f4
Елизавета <sova.lizochka05@gmail.com>
2024-11-20 22:19:52 +0300

    Add files via upload

---
+++ Task1_34.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+
+static double solution(double a, double b) {
+	if (a == 0) {
+		std::cout << "error" << std::endl;
+		return 0 ;
+	}
+	return -b / a;
+
+}
+
+int main()
+{
+	double a = 8;
+	double b = 9;
+	std::cout <<solution(a, b)<< std::endl;
+	return 0;
+}
