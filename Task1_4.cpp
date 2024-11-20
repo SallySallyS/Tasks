cef8ac74e46da5c3c1ad218a021bec614ab983d6
Елизавета <sova.lizochka05@gmail.com>
2024-11-20 22:38:10 +0300

    Add files via upload

---
+++ Task1_4.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <sstream>
+
+//1.4.Составить программу вывода на экран числа, вводимого с клавиатуры.По -
+//сле выводимого числа должно следовать сообщение " - вот какое число Вы ввели".
+
+int main()
+{
+	std::cout << "Enter: ";
+	std::string aString;
+
+	std::cin >> aString;
+
+	int a = std::stoi(aString);
+	std::cout << "You enter: " << a << std::endl;
+
+
+}
+
