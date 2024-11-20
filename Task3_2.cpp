f6295b0880357557056de085c5acf41da3670dd7
Елизавета <sova.lizochka05@gmail.com>
2024-11-21 00:03:53 +0300

    Add files via upload

---
+++ Task3_2.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+//Вычислить значение логического выражения при следующих значениях логи -
+//ческих величин X, Y и Z : X = Ложь, Y = Истина, Z = Ложь :
+//
+//	а) X или Z; б) X и Y; c) X и Z.
+
+int main()
+{
+	bool x = false;
+	bool y = true;
+	bool z = false;
+
+	bool a = x || z;
+	bool b = x && y;
+	bool c = x && z;
+
+	std::cout << a << b << c << std::endl;
+	return 0;
+}
