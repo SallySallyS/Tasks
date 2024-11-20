cb7bae04e9702d341b18cf1ad8fc7a739d6e4a20
Елизавета <sova.lizochka05@gmail.com>
2024-11-20 22:59:20 +0300

    Add files via upload

---
+++ Task1_25.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <sstream>
+//Дана сторона квадрата.Найти его периметр.
+static double perimeter(double a) {
+	return 4 * a;
+}
+int main()
+{
+	std::cout << "Enter: ";
+	std::string aString;
+	std::cin >> aString;
+	double a = std::stoi(aString);
+	std::cout <<"Perimeter: "<< perimeter(a) << std::endl;
+}
+
