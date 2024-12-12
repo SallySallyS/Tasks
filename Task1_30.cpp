b684bb3aeb06c69e4f50c450014378b00e001ea9
Елизавета <sova.lizochka05@gmail.com>
2024-12-12 20:27:52 +0300

    Update Task1_30.cpp

--- Task1_30.cpp
+++ Task1_30.cpp
@@ -1,5 +1,6 @@
 ﻿
 #include <iostream>
+#include<sstream>
 
 //а) вычисления значения функции
 //z = x * x * x - 2.5 * x * y + 1.78 * x * x -2.5 * y + 1 
@@ -22,13 +23,31 @@ static double x(double a, double b) {
 int main()
 {
 	{
-		double x = 12;
-		double y = 32.1;
+		//double x = 12;
+		//double y = 32.1;
+		
+		std::string xString;
+		std::string yString;
+		std::cout << "Enter x: " << std::endl;
+		std::cin >> xString;
+		std::cout << "Enter y: " << std::endl;
+		std::cin >> yString;
+		double x = std::stoi(xString);
+		double y = std::stoi(yString);
 		std::cout << z(x,y) << std::endl;
 	}
 	{
-		double a = 163;
-		double b = 98.5;
+		//double a = 163;
+		//double b = 98.5;
+		
+		std::string aString;
+		std::string bString;
+		std::cout << "Enter a: " << std::endl;
+		std::cin >> aString;
+		std::cout << "Enter b: " << std::endl;
+		std::cin >> bString;
+		double a = std::stoi(aString);
+		double b = std::stoi(bString);
 		std::cout << x(a, b) << std::endl;
 
 	}
